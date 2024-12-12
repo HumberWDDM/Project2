b5b4426bccb2d53ecb705cee2e4e2ad570c2bf55
hwajin <57221951+leehj00@users.noreply.github.com>
2024-12-12 10:43:30 -0500

    added css and image file

---
+++ pro2/src/components/Features.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Feature = () => {
+    return (
+        <section class="features">
+        <div class="fea_container">
+            <span class="features_span">Features</span>
+            <h3>Why people choose us?</h3>
+            <p>Porro eveniet, autem ipsam vitae consequatur!</p>
+        </div>
+        </section>
+    );
+}
+
+export default Feature;
