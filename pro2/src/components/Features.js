be961ab4033c7046f59ec1e4725d34bb0b487632
Trust Ezekiel-Okezie <Trustezekiel.okezie@gmail.com>
2024-12-13 18:05:22 +0100

    updated the features component to match the original project file

--- pro2/src/components/Features.js
+++ pro2/src/components/Features.js
@@ -1,58 +1,63 @@
 import React from "react";
-import "../styles/Features.css"
+import "../styles/Features.css";
 
 const Feature = () => {
+    const features = [
+        {
+            imgSrc: "./images/icon-serv-1-256x256.png",
+            h4Msg: "Menu for every taste",
+            pMsg: (
+                <>
+                    Dolor sit amet, consectetur adipisicing elit et
+                    <br />
+                    molestias possimus
+                </>
+            ),
+        },
+        {
+            imgSrc: "./images/icon-serv-2-256x256.png",
+            h4Msg: "Always fresh ingredients",
+            pMsg: (
+                <>
+                    Assumenda possimus eaque illo iste, autem.
+                    <br />
+                    Porro eveniet autem
+                </>
+            ),
+        },
+        {
+            imgSrc: "./images/icon-serv-3-256x256.png",
+            h4Msg: "Experienced chefs",
+            pMsg: (
+                <>
+                    Rolorem, beatae dolorum, praesentium itaque et
+                    <br />
+                    quam quaerat
+                </>
+            ),
+        },
+    ];
+
     return (
-        <section class="features">
-        <div class="fea_container">
-            <span class="features_span">Features</span>
-            <h3>Why people choose us?</h3>
-            <p>Porro eveniet, autem ipsam vitae consequatur!</p>
-        </div>
+        <section className="features">
+            <div className="fea_container">
+                <span className="features_span">Features</span>
+                <h3>Why people choose us?</h3>
+                <p>Porro eveniet, autem ipsam vitae consequatur!</p>
+            </div>
+
+            <div className="grid-features">
+                {features.map((feature, index) => (
+                    <div key={index} className={`f f${index + 1}`}>
+                        <img src={feature.imgSrc} alt={feature.h4Msg} />
+                        <h4>{feature.h4Msg}</h4>
+                        <p>{feature.pMsg}</p>
+                    </div>
+                ))}
+            </div>
+
         </section>
     );
-}
-
-const GridFeature = ({type}) => {
-    const imgSrcs = 
-    {
-        first: "./images/icon-serv-1-256x256.png",
-        second: "./images/icon-serv-2-256x256.png",
-        third: "./images/icon-serv-3-256x256.png",
-    };
-
-    const h4Msg = 
-    {
-        first: "Menu for every taste",
-        second: "Always fresh ingredients",
-        third: "Experienced chefs",
-    };
-
-    const pMsg = 
-    {
-        first: 
-        <>
-        Dolor sit amet, consectetur adipisicing elit et<br/>molestias possimus
-        </>,
-        second:
-        <>
-        Assumenda possimus eaque illo iste, autem.<br/>Porro eveniet autem
-        </>,
-        third:
-        <>
-        Rolorem, beatae dolorum, praesentium itaque et<br/>quam quaerat
-        </>
-    };
-
-    return (
-        
-        <div class="f f1">
-            <img src={imgSrcs[type]} alt=""></img>
-            <h4>{h4Msg[type]}</h4>
-            <p>{pMsg[type]}</p>
-        </div>
-    )
-}
+};
 
 export default Feature;
-export {GridFeature};
