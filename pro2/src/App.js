4c72b0c4980ef9a9d2005a4bf66b474bdae210c9
Trust Ezekiel-Okezie <Trustezekiel.okezie@gmail.com>
2024-12-13 19:16:53 +0100

    made updates

--- pro2/src/App.js
+++ pro2/src/App.js
@@ -1,21 +1,15 @@
 import React from "react"
 import Feature from "./components/Features"
-import Footer from "../src/components/Footer"
-import IframeSection from "../src/components/IframeVideo"
-import Newsletter from "../src/components/NewsLetter"
-import VideoSection from "../src/components/VideoSection"
+import Footer from "./components/Footer"
+import IframeSection from "./components/IframeVideo"
+import Newsletter from "./components/NewsLetter"
+import VideoSection from "./components/VideoSection"
 import "./App.css"
 
 function App() {
   return (
     <div>
       <Feature/>
-      {/* <div class="features__grid">
-        <GridFeature type={0}/>
-        <GridFeature type={1}/>
-        <GridFeature type={3}/>
-      </div> */}
-
       <VideoSection />
       <Newsletter />
       <IframeSection />
