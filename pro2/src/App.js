e55fc55f94b1081b77041bb269dfd03b3d44c7c6
Trust Ezekiel-Okezie <Trustezekiel.okezie@gmail.com>
2024-12-13 16:52:01 +0100

    removed unnecessary import

--- pro2/src/App.js
+++ pro2/src/App.js
@@ -1,5 +1,4 @@
 import React from "react"
-import ReactDOM from "react-dom"
 import Feature from "./components/Features"
 import { GridFeature } from "./components/Features"
 import Footer from "../src/components/Footer"
